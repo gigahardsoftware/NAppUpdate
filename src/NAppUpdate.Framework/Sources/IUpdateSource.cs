8e2091864512d5251fd8b7621064b10c8d82ad02
Aleksa Trajkovic <aleksatr@gmail.com>
2017-10-17 11:07:40 +0200

    Added [ServiceContract] and [OperationContract] attributes over IUpdateSource methods, so that WCF service can be used as updates source.

--- src/NAppUpdate.Framework/Sources/IUpdateSource.cs
+++ src/NAppUpdate.Framework/Sources/IUpdateSource.cs
@@ -1,11 +1,16 @@
 ﻿using System;
 using NAppUpdate.Framework.Common;
+using System.ServiceModel;
 
 namespace NAppUpdate.Framework.Sources
 {
+	[ServiceContract]
 	public interface IUpdateSource
 	{
+		[OperationContract]
 		string GetUpdatesFeed(); // TODO: return a the feed as a stream
+
+		[OperationContract]
 		bool GetData(string filePath, string basePath, Action<UpdateProgressInfo> onProgress, ref string tempLocation);
 	}
 }
